0bd5e226e7c86ce7508b26c76547b6bede592469
Derek Springer <derekspringer@gmail.com>
2011-09-20 15:49:59 -0700

    changed readme to markdown

---
+++ README.mk
@@ -0,0 +1,16 @@
+LabView VIs
+==================
+
+A collection of helpful LabView VIs I've put together that others might find useful.
+
+Dependencies
+------------
+* [OpenG](http://wiki.openg.org)
+* [OpenG LabVIEW Data Library](http://wiki.openg.org/Oglib_lvdata)
+
+UTC Unix Time
+-----------------------------
+* Returns current time in Unix UTC format
+** 64-bit whole second 
+** double fractional second
+
